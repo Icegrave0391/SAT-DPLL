a4658b2dcf0662b2caddc60df118cec0bf3a6bab
Icegrave0391 <849680403@qq.com>
2019-02-18 18:39:04 +0800

    Initial Commit

---
+++ CourseDesign/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CourseDesign
+//
+//  Created by 张储祺 on 2019/2/18.
+//  Copyright © 2019 icegrave0391. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
